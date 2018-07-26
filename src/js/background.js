a3243bfc6a593254ac8ab2599a51466509262467
Rom <skav@gmx.fr>
2018-07-26 14:55:35 +0200

    Update and rename src/background.js to src/js/background.js

---
+++ src/js/background.js
@@ -0,0 +1,24 @@
+function Download500pximgs() {	
+
+	// Check if files exist, if yes, rename them, if no continue
+
+    const req = new XMLHttpRequest();
+    req.open('GET', 'http://feed.500px.com/500px-best', false);
+    req.send(null);
+
+    if (req.status === 200) {
+        
+    }
+
+    var fs = require('fs');
+    fs.rename('/walls/Image1.jpeg', '/walls/Image2.png', function (err) {
+        if (err) console.log('ERROR: ' + err);
+    });
+
+}
+setInterval(Download500pximgs, 10800000);
+
+function openPage() {
+    browser.popup.create({
+
+    })
