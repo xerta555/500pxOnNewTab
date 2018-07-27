17331f03dc8d00d83fab92c1ce5d07d098b35b11
Rom <skav@gmx.fr>
2018-07-27 22:46:06 +0200

    Update newtab.js

--- src/js/newtab.js
+++ src/js/newtab.js
@@ -1,8 +1,18 @@
+const NEW_TAB_PAGE = 'html/newtab.html';
 
-  background_color : '#ffffff',
-  focus_website : true,
-  local_file : '/walls/main.jpg',
-  storage_schema : '1',
-  type : 'custom_url',
-  url : 'about:newtab'
-};
+{
+    background_color: '#ffffff',
+        focus_website : true,
+            local_file : '/js/settings.js',
+                storage_schema : '1',
+                    type : 'custom_url',
+                        url : 'about:newtab'
+
+    var title = browser.i18n.getMessage("NewTabTitle");
+
+    browser.menus.create(
+        createProperties, // command
+        function () { _execute_browser_action } // icons: {"32": "img/image32.png"}
+    )
+}
+
