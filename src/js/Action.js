ae35adf54b5a85e57ab847503b4458e04beb2dc9
Rom <skav@gmx.fr>
2018-07-29 18:39:17 +0200

    Update Action.js

--- src/js/Action.js
+++ src/js/Action.js
@@ -1,30 +1,13 @@
-/**
- * CSS to hide everything on the page,
- * except for elements that have the "beastify-image" class.
- */
-const hidePage = `body > :not(.beastify-image) {
-                    display: none;
-                  }`;
+var renderPopup = function () {
+    var elem, text;
 
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
+    if (popupData.tabTitle) {
+        document.title = popupData.appname + ' - ' + popupData.tabTitle;
+    }
 
-        /**
-         * Given the name of a beast, get the URL to the corresponding image.
-         */
-        function beastNameToURL(beastName) {
-            switch (beastName) {
-                case "Frog":
-                    return browser.extension.getURL("beasts/frog.jpg");
-                case "Snake":
-                    return browser.extension.getURL("beasts/snake.jpg");
-                case "Turtle":
-                    return browser.extension.getURL("beasts/turtle.jpg");
-            }
-        }
-    },
- },
+    elem = document.body;
+    elem.classList.toggle('advancedUser', popupData.advancedUserEnabled);
+    elem.classList.toggle(
+        'off',
+        popupData.pageURL === '' || !popupData.netFilteringSwitch
+    );
