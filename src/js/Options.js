895becb3f546a36f2ba10d561afb654b49344ad4
Rom <skav@gmx.fr>
2018-09-01 00:16:14 +0200

    Update Options.js

--- src/js/Options.js
+++ src/js/Options.js
@@ -6,6 +6,37 @@ const options = {
 
 };
 
+
+
+var enteredText = document.getElementById("settings-intro").value;
+var numberOfLineBreaks = (enteredText.match(/\n/g) || []).length;
+alert('Number of breaks: ' + numberOfLineBreaks);
+
+
+
+var enteredText = document.getElementById("auto-option").value;
+var numberOfLineBreaks = (enteredText.match(/\n/g) || []).length;
+alert('Number of breaks: ' + numberOfLineBreaks);
+
+
+
+var enteredText = document.getElementById("perm-req-text").value;
+var numberOfLineBreaks = (enteredText.match(/\n/g) || []).length;
+alert('Number of breaks: ' + numberOfLineBreaks);
+
+
+
+Settings.get('showOptionsInDedicatedTab').then(async v => {
+    if (!v) return
+    const tab = await browser.tabs.getCurrent()
+    if (!tab || !tab.url ||
+        tab.url.toLowerCase().startsWith(location.origin.toLowerCase())) return
+    await browser.tabs.create({ url: location.href })
+    location.href = 'about:blank'
+})
+
+
+
 let typeSelect = document.getElementById("type")
 typeSelect.addEventListener("change", function () {
     let admOptionValue = document.getElementById("local").value;
@@ -16,3 +47,5 @@ typeSelect.addEventListener("change", function () {
         document.getElementById("admDivCheck").style.display = "none";
     }
 });
+
+
