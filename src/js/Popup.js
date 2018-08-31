76d6848ecb4c19af97160c7433269c64fd5ae473
Rom <skav@gmx.fr>
2018-09-01 00:15:44 +0200

    Update Popup.js

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,6 +1,7 @@
-document.querySelector('#options').addEventListener('click', async () => {
-    if (await Settings.get('showOptionsInDedicatedTab'))
-        void browser.tabs.create({ url: browser.runtime.getURL('options.html') });
+document.querySelector('#clickSettings').addEventListener('click', async (event) => {
+    event.preventDefault();
+    if ((await browser.storage.local.get()).showOptionsInDedicatedTab)
+        void browser.tabs.create({ url: browser.runtime.getURL('/html/Options.html') });
     else
         void browser.runtime.openOptionsPage();
 });
