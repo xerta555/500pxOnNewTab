f9a0323db4677134a8e7e2e8f5b1fe1525321877
Rom <skav@gmx.fr>
2018-09-05 00:01:57 +0200

    Update Action.js

--- src/js/Action.js
+++ src/js/Action.js
@@ -22,3 +22,11 @@ chrome.tabs.query(queryInfo, function (tabs) {
         }
         }
 }
+
+document.querySelector('#clickSettings').addEventListener('click', async (event) => {
+    event.preventDefault();
+        if ((await browser.storage.local.get()).showOptionsInDedicatedTab)
+            void browser.tabs.create({ url: browser.runtime.getURL('/html/Options.html') });
+        else
+            void browser.runtime.openOptionsPage();
+});
