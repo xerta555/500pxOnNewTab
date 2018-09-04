130d2074553eb454bd1cc3fb8150785b74722918
Rom <skav@gmx.fr>
2018-09-05 00:01:40 +0200

    Delete Popup.js

--- src/js/Popup.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#clickSettings').addEventListener('click', async (event) => {
-    event.preventDefault();
-    if ((await browser.storage.local.get()).showOptionsInDedicatedTab)
-        void browser.tabs.create({ url: browser.runtime.getURL('/html/Options.html') });
-    else
-        void browser.runtime.openOptionsPage();
-});
